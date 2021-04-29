25eda5655ca4ffb5d494e67b6526ae916a1b6fb6
Alexa Stefankiewicz <alexa.c.stef@gmail.com>
2021-04-28 18:10:23 -0700

    responsive header complete, home page started

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -4,6 +4,41 @@ const Home = () => {
   return (
     <div>
       <div className="hero-image"></div>
+      <section className="intro-section">
+        <div className="intro-card">
+          <div className="card-body">
+            <h3 className="card-title">
+              Good Food
+            </h3>
+            <p className="card-text">
+              Some blurb about the food. Maybe three sentences. Photo of the pizza.
+            </p>
+            <a className="card-link" href="3">View Menu</a>
+          </div>
+        </div>
+        <div className="intro-card">
+          <div className="card-body">
+            <h3 className="card-title">
+              Good Drinks
+            </h3>
+            <p className="card-text">
+              Some blurb about the beer and drinks. Maybe three sentences. Photo of a beer. Include info about happy hour?
+            </p>
+            <a className="card-link" href="3">View Drinks</a>
+          </div>
+        </div>
+        <div className="intro-card">
+          <div className="card-body">
+            <h3 className="card-title">
+              Good Times
+            </h3>
+            <p className="card-text">
+              Some blurb about the specials and events. Maybe three sentences. Photo of something, doesn't matter. Link this to the instagram
+            </p>
+            <a className="card-link" href="3">Learn More</a>
+          </div>
+        </div>
+      </section>
     </div>
   )
 }
