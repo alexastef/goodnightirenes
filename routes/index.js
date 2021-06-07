86fe707a0cbc2fba2281410a7f7780fb7d3059d0
Alexa Stefankiewicz <alexa.c.stef@gmail.com>
2021-06-06 20:12:05 -0700

    routes connected but not returning data

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,19 @@
-module.exports = app => {
-  const menuController = require('../controller/menuController');
-  const router = require('express').Router();
+// module.exports = app => {
+//   const menuController = require('../controller/menuController');
+//   const router = require('express').Router();
 
-  router.get("/", menuController.findAll);
+//   router.get("/", menuController.findAll);
 
-  app.use('/api/beer', router);
-}
+//   app.use('/api/beer', router);
+// }
+const path = require('path');
+const router = require('express').Router();
+const apiRoutes = require('./api');
+
+router.use('/api', apiRoutes);
+
+router.use((req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
+
+module.exports = router;
